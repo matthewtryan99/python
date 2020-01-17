cac927c7153ec58e0a250f03f1bb99af48e32ef8
Matt Ryan <matthewtryan99@gmail.com>
2020-01-16 21:00:26 -0600

    medium question 2 exercise3

--- exercise3.py
+++ exercise3.py
@@ -1,8 +1,20 @@
-def smallest_number(lst):
-    smallest = lst[0]
-    for i in lst:
-        if i < smallest:
-            smallest = i
-    return smallest
+###Medium Questions
+##Question 1
+# def smallest_number(lst):
+#     smallest = lst[0]
+#     for i in lst:
+#         if i < smallest:
+#             smallest = i
+#     return smallest
 
-print(smallest_number([1,2,4,7,5,3,5,]))
+# print(smallest_number([1,2,4,7,5,3,5,]))
+
+##Question 2
+def largest_number(lst):
+    largest = lst[0]
+    for i in lst:
+        if i > largest:
+            largest = i
+    return largest
+
+print(largest_number([1,2,4,7,5,3,5,]))
