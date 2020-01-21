20b0b67d5193e5b4cd3d027a7abd81d1aefad93f
Matt Ryan <matthewtryan99@gmail.com>
2020-01-20 22:27:26 -0600

    exercise 4 medium question 2

--- exercise4.py
+++ exercise4.py
@@ -1,13 +1,36 @@
 ###Medium Questions
 ##Question 1
-input_word = input("Input a word: ")
-input_list = list(input_word)
-my_dict = {}
+# input_word = input("Input a word: ")
+# input_list = list(input_word)
+# my_dict = {}
 
-for i in range(len(input_list)):
-    if input_list[i] not in my_dict:
-        my_dict[input_list[i]] = 1
-    else:
-        my_dict[input_list[i]] += 1
+# for i in range(len(input_list)):
+#     if input_list[i] not in my_dict:
+#         my_dict[input_list[i]] = 1
+#     else:
+#         my_dict[input_list[i]] += 1
 
-print(my_dict)
+# print(my_dict)
+
+##Question 2
+ramit = {
+'name': 'Ramit',
+'email': 'ramit@gmail.com',
+'interests': ['movies', 'tennis'],
+'friends': [
+    {
+    'name': 'Jasmine',
+    'email': 'jasmine@yahoo.com',
+    'interests': ['photography', 'tennis']
+    },
+    {
+    'name': 'Jan',
+    'email': 'jan@hotmail.com',
+    'interests': ['movies', 'tv']
+    }
+]
+}
+print(ramit['email'])
+print(ramit['interests'][0])
+print(ramit['friends'][0]['email'])
+print(ramit['friends'][1]['interests'][1])
