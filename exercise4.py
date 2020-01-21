cdf7cdc6b0e3d9c39afa0f14170b0a2639534a02
Matt Ryan <matthewtryan99@gmail.com>
2020-01-21 09:20:29 -0600

    real medium question 2

--- exercise4.py
+++ exercise4.py
@@ -12,36 +12,49 @@
 
 # print(my_dict)
 
-##Question 2
-ramit = {
-'name': 'Ramit',
-'email': 'ramit@gmail.com',
-'interests': ['movies', 'tennis'],
-'friends': [
-    {
-    'name': 'Jasmine',
-    'email': 'jasmine@yahoo.com',
-    'interests': ['photography', 'tennis']
-    },
-    {
-    'name': 'Jan',
-    'email': 'jan@hotmail.com',
-    'interests': ['movies', 'tv']
-    }
-]
-}
-# print(ramit['email'])
-# print(ramit['interests'][0])
-# print(ramit['friends'][0]['email'])
-# print(ramit['friends'][1]['interests'][1])
+# ##Question 2
+# ramit = {
+# 'name': 'Ramit',
+# 'email': 'ramit@gmail.com',
+# 'interests': ['movies', 'tennis'],
+# 'friends': [
+#     {
+#     'name': 'Jasmine',
+#     'email': 'jasmine@yahoo.com',
+#     'interests': ['photography', 'tennis']
+#     },
+#     {
+#     'name': 'Jan',
+#     'email': 'jan@hotmail.com',
+#     'interests': ['movies', 'tv']
+#     }
+# ]
+# }
+#  print(ramit['email'])
+#  print(ramit['interests'][0])
+#  print(ramit['friends'][0]['email'])
+#  print(ramit['friends'][1]['interests'][1])
 
 ##Question 3
-def friend_count(diction):
-    diction['friend_count'] = 0
-    for i in range(len(diction['friends'])):
-        diction['friend_count'] += 1
-    return diction
+# def friend_count(diction):
+#     diction['friend_count'] = 0
+#     for i in range(len(diction['friends'])):
+#         diction['friend_count'] += 1
+#     return diction
 
-new_dict = friend_count(ramit)
+# new_dict = friend_count(ramit)
 
-print(new_dict['friend_count'])
+# print(new_dict['friend_count'])
+
+##Question 2
+input_word = input("Input a sentence: ")
+input_list = input_word.split()
+my_dict = {}
+
+for i in range(len(input_list)):
+    if input_list[i] not in my_dict:
+        my_dict[input_list[i]] = 1
+    else:
+        my_dict[input_list[i]] += 1
+
+print(my_dict)
