1048871b71180324050e3c9cebd4d077603b3549
Matt Ryan <matthewtryan99@gmail.com>
2020-01-22 10:45:35 -0600

    class set up

--- exercise6.py
+++ exercise6.py
@@ -1,1 +1,16 @@
 ##Banking App
+class AccountHolder:
+    def __init__(self, fname, mname, lname, account, balance, status):
+        self.fname = fname
+        self.lname = lname
+        self.account = account
+        self.balance = balance
+        self.status = status
+
+class Bank:
+    def __init__(self):
+        self.account_holder = []
+    def add_account_holder(self, fname, mname, lname, account, balance, status):
+        temp = AccountHolder(fname, mname, lname, account, balance, status)
+        self.account_holder.append(temp)
+        
