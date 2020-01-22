36efe4071697300c3cdcbc13b02072ae672dc856
Matt Ryan <matthewtryan99@gmail.com>
2020-01-22 13:14:52 -0600

    close account/deposit money

--- exercise6.py
+++ exercise6.py
@@ -13,8 +13,9 @@ class Bank:
         self.name = name
         self.address = address
         self.account_holder = []
+        
     def add_account_holder(self, fname, mname, lname, account, balance, status):
-        if balance > 100:
+        if balance >= 100:
             temp = AccountHolder(fname, mname, lname, account, balance, status)
             self.account_holder.append(temp)
             return f"A {account} account created for {fname} {mname} {lname} with a balance of {balance}."
@@ -28,21 +29,43 @@ class Bank:
     def transfer(self, name1, name2, transfer_amount):
         for i in self.account_holder:
             for x in self.account_holder:
-                if name1 == i.fname and name2 == x.fname:\
-                    if i.balance >= transfer_amount:
-                        i.balance -= transfer_amount
-                        x.balance += transfer_amount
+                if name1 == i.fname and name2 == x.fname:
+                    if i.status != 'freeze' and i.status != 'closed':
+                        if i.balance >= transfer_amount:
+                            i.balance -= transfer_amount
+                            x.balance += transfer_amount
+                        else:
+                            i.balance -= transfer_amount
+                            x.balance += transfer_amount
+                            i.balance -= 35
+                            i.status = 'freeze'
                     else:
-                        print("insufficient funds")
+                        break
 
     def withdrawl(self, name, withdrawl_amount):
         for i in self.account_holder:
             if name == i.fname:
-                if i.balance >= withdrawl_amount:
-                    i.balance -= withdrawl_amount
+                if i.status != 'freeze' and i.status != 'closed':
+                    if i.balance >= withdrawl_amount:
+                        i.balance -= withdrawl_amount
+                    else:
+                        i.balance -= withdrawl_amount
+                        i.balance -= 35
+                        i.status = 'freeze'
                 else:
-                    print("insufficient funds")
+                    break
 
+    def closeAccount(self, name):
+        for i in self.account_holder:
+            if i.fname == name:
+                i.status = 'closed'
+
+    def depositMoney(self, name, deposit_amount):
+        for i in self.account_holder:
+            if i.fname == name and i.status != 'closed':
+                i.balance += deposit_amount
+            if i.status == 'freeze' and i.balance > 0:
+                i.status = 'opened'
 
 def main():
     wellsFargo = Bank('wells fargo', '123 sesame street')
@@ -52,7 +75,9 @@ def main():
         print("2. Check Account Holders")
         print("3. Transfer Balance")
         print("4. Withdrawl Balance")
-        print("6. Exit Application")
+        print("5. Deposit")
+        print("6. Close Account")
+        print("7. Exit Application")
 
         action = int(input("What would you like to do?"))
 
@@ -73,8 +98,15 @@ def main():
         elif(action == 4):
             name = input("Enter your first name: ")
             withdrawl_amount = int(input("Enter your desired withdrawl amount: "))
+            wellsFargo.withdrawl(name, withdrawl_amount)
+        elif (action == 5):
+            name = input("Enter the first name of the account you wish to deposit to: ")
+            deposit_amount = int(input("Enter the amount Depositing: "))
+            wellsFargo.depositMoney(name, deposit_amount)
         elif (action == 6):
+            name = input("Enter your first name: ")
+            wellsFargo.closeAccount(name)
+        elif (action == 7):
             break
-    
 
 main()
